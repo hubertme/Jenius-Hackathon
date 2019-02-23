82f217b798df8b08b32680611285b60f77e62c43
Hubert Wang <hubert.tatra@gmail.com>
2019-02-23 17:27:47 +0700

    Add merchant VC raw files

---
+++ Jenius-Hackathon/Merchant/Controller/MerchantViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MerchantViewController.swift
+//  Jenius-Hackathon
+//
+//  Created by Hubert Wang on 23/02/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class MerchantViewController: UIViewController {
+    
+    // MARK: - Attributes
+    
+    // MARK: - Outlets
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Private methods
+
+}
