997e604205d825bc0117b82161a3458bafbe515e
Andy Jiehan Aldicho <emanresuser@gmail.com>
2019-02-23 10:00:37 +0700

    restructuring files

---
+++ Jenius-Hackathon/PaymentVC/PaymentViewController-Layout.swift
@@ -0,0 +1,9 @@
+//
+//  PaymentViewController - Layout.swift
+//  Jenius-Hackathon
+//
+//  Created by Andy Aldicho on 23/02/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import Foundation
