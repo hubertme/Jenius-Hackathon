c6a7f5e41d0292a8a6f9f068a5249a018591752d
Hubert Wang <hubert.tatra@gmail.com>
2019-02-23 10:42:07 +0700

    Adjust make single line UITextField extension

--- Jenius-Hackathon/Extension/UITextField.swift
+++ Jenius-Hackathon/Extension/UITextField.swift
@@ -12,10 +12,14 @@ import UIKit
 extension UITextField {
     func makeSingleLine() {
         self.borderStyle = .none
-        self.layer.backgroundColor = UIColor.white.cgColor
-        self.layer.shadowColor = mainColor.cgColor
+        self.layer.backgroundColor = mainColor.cgColor
+        self.layer.shadowColor = UIColor.white.cgColor
         self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
         self.layer.shadowOpacity = 1.0
         self.layer.shadowRadius = 0
+        
+        self.attributedPlaceholder = NSAttributedString(string: self.placeholder!, attributes: [
+            NSAttributedString.Key.foregroundColor : #colorLiteral(red: 0.8500000238, green: 0.8500000238, blue: 0.8500000238, alpha: 1)
+            ])
     }
 }
