3b752509bef708ce84aeef658ffaf929d59ae983
Hubert Wang <hubert.tatra@gmail.com>
2019-02-23 10:06:15 +0700

    Add login VC raw files

---
+++ Jenius-Hackathon/Landing/View/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Jenius-Hackathon
+//
+//  Created by Hubert Wang on 23/02/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
