410f7c9daa0e52240d284389378b7b66b5db3950
Hubert Wang <hubert.tatra@gmail.com>
2019-02-24 06:40:14 +0700

    Add forget password VC raw files

---
+++ Jenius-Hackathon/Passwords/Controller/ForgetPasswordViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ForgetPasswordViewController.swift
+//  Jenius-Hackathon
+//
+//  Created by Hubert Wang on 24/02/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class ForgetPasswordViewController: UIViewController {
+    
+    // MARK: - 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
