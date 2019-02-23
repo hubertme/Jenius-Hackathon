39e00a89fc0b78969c657c27b81d3d63f47416f7
Andy Jiehan Aldicho <emanresuser@gmail.com>
2019-02-23 10:15:49 +0700

    added layout ui function - paymentVC

--- Jenius-Hackathon/PaymentVC/PaymentViewController-Layout.swift
+++ Jenius-Hackathon/PaymentVC/PaymentViewController-Layout.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 Hubert Wang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension PaymentViewController {
+    func layoutUI() {
+        self.layoutTitleContainer()
+        self.layoutTitleLabel()
+        self.layoutMerchantIcon()
+    }
+    
+    private func layoutTitleContainer() {
+        [titleContainer.widthAnchor.constraint(equalToConstant: view.frame.width),
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        titleLabel.heightAnchor.constraint(equalToConstant: 40)].forEach({$0.isActive = true})
+    }
+    
+    private func layoutTitleLabel() {
+        [titleLabel.centerXAnchor.constraint(equalTo: titleContainer.centerXAnchor),
+         titleLabel.centerYAnchor.constraint(equalTo: titleContainer.centerYAnchor)].forEach({$0.isActive = true})
+    }
+    
+    private func layoutMerchantIcon() {
+        let size: CGFloat = 20
+        [merchantIcon.leadingAnchor.constraint(equalTo: titleContainer.leadingAnchor, constant: 20),
+        merchantIcon.centerYAnchor.constraint(equalTo: titleContainer.centerYAnchor),
+        merchantIcon.widthAnchor.constraint(equalToConstant: size),
+        merchantIcon.heightAnchor.constraint(equalToConstant: size)].forEach({$0.isActive = true})
+        
+    }
+}
