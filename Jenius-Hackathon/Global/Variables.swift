04310aca0469475b7a90d60ca4182b5695a65ede
Hubert Wang <hubert.tatra@gmail.com>
2019-02-23 10:14:33 +0700

    Add main colour to global variables

---
+++ Jenius-Hackathon/Global/Variables.swift
@@ -0,0 +1,12 @@
+//
+//  Variables.swift
+//  Jenius-Hackathon
+//
+//  Created by Hubert Wang on 23/02/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let mainColor = #colorLiteral(red: 0, green: 0.6431372549, blue: 0.8705882353, alpha: 1)
