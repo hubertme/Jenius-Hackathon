eed7513ec8c5c7dfc0a105581fc6dae4f3ce2f18
Hubert Wang <hubert.tatra@gmail.com>
2019-02-23 22:41:19 +0700

    Add header + section header cells

--- Jenius-Hackathon/Merchant/Controller/MerchantViewController.swift
+++ Jenius-Hackathon/Merchant/Controller/MerchantViewController.swift
@@ -28,21 +28,62 @@ class MerchantViewController: UIViewController {
     private func setupElements() {
         self.contentTableView.delegate = self
         self.contentTableView.dataSource = self
+        
+        self.contentTableView.register(HeaderCell.nib, forCellReuseIdentifier: HeaderCell.cellDescription)
     }
 }
 
-extension MerchantViewController: UITableViewDelegate, UITableViewDataSource {
+extension MerchantViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 4
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 3
+        case 2:
+            return 5
+        case 3:
+            return 2
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return (indexPath.section != 0 && indexPath.row == 0) ? 50 : UITableView.automaticDimension
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let headerCell = contentTableView.dequeueReusableCell(withIdentifier: HeaderCell.cellDescription, for: indexPath) as! HeaderCell
+            return headerCell
+        } else if indexPath.row == 0 {
+            let sectionHeaderCell = UITableViewCell(style: .default, reuseIdentifier: "sectionHeaderCell")
+            sectionHeaderCell.textLabel?.textColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+            sectionHeaderCell.backgroundColor = #colorLiteral(red: 0.8500000238, green: 0.8500000238, blue: 0.8500000238, alpha: 1)
+            sectionHeaderCell.selectionStyle = .none
+            
+            switch indexPath.section {
+            case 1:
+                sectionHeaderCell.textLabel?.text = "Merchant information"
+            case 2:
+                sectionHeaderCell.textLabel?.text = "Settings"
+            default:
+                sectionHeaderCell.textLabel?.text = ""
+            }
+            
+            return sectionHeaderCell
+        }
         return UITableViewCell()
     }
+    
+}
+
+extension MerchantViewController: UITableViewDelegate {
     
 }
