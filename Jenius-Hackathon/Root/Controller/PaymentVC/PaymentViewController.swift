c5d1c5811c77d562b2a78cc4abc19babc0e17b06
Andy Jiehan Aldicho <emanresuser@gmail.com>
2019-02-23 09:53:20 +0700

    init paymentvc

---
+++ Jenius-Hackathon/Root/Controller/PaymentVC/PaymentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentViewController.swift
+//  Jenius-Hackathon
+//
+//  Created by Andy Aldicho on 23/02/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+    
+    var titleContainer: UIView!
+    var titleLabel: UILabel!
+    var merchantIcon: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
