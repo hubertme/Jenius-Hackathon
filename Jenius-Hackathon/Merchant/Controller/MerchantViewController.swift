5a39ecedb9882282606de8a0cf3582444ecafa8b
Hubert Wang <hubert.tatra@gmail.com>
2019-02-23 21:26:23 +0700

    Add table view raw view

--- Jenius-Hackathon/Merchant/Controller/MerchantViewController.swift
+++ Jenius-Hackathon/Merchant/Controller/MerchantViewController.swift
@@ -13,15 +13,36 @@ class MerchantViewController: UIViewController {
     // MARK: - Attributes
     
     // MARK: - Outlets
+    @IBOutlet weak var contentTableView: UITableView!
     
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setupElements()
     }
     
     // MARK: - Actions
     
     // MARK: - Private methods
+    private func setupElements() {
+        self.contentTableView.delegate = self
+        self.contentTableView.dataSource = self
+    }
+}
 
+extension MerchantViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
