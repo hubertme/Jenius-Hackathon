7cf638fb6323c4716f03da1c55d04bac781ca734
Hubert Wang <hubert.tatra@gmail.com>
2019-02-24 07:03:29 +0700

    Setup forget password VC elements

--- Jenius-Hackathon/Passwords/Controller/ForgetPasswordViewController.swift
+++ Jenius-Hackathon/Passwords/Controller/ForgetPasswordViewController.swift
@@ -10,12 +10,57 @@ import UIKit
 
 class ForgetPasswordViewController: UIViewController {
     
-    // MARK: - 
+    // MARK: - Attributes
+    
+    // MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var doneButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.setupElements()
+    }
+    
+    // MARK: - Private methods
+    private func setupElements() {
+        self.view.backgroundColor = mainColor
+        
+        self.emailTextField.makeSingleLine()
+        self.emailTextField.delegate = self
+        
+        self.doneButton.setTitleColor(mainColor, for: .normal)
+        self.doneButton.clipsToBounds = true
+        self.doneButton.layer.cornerRadius = 8
+        self.doneButton.alpha = 0.2
+        self.doneButton.isUserInteractionEnabled = false
+    }
+    
+    private func animateDoneButton() {
+        if self.emailTextField.text != "" {
+            UIView.animate(withDuration: 1) {
+                self.doneButton.alpha = 1
+                self.doneButton.isUserInteractionEnabled = true
+            }
+        } else {
+            UIView.animate(withDuration: 1) {
+                self.doneButton.alpha = 0.2
+                self.doneButton.isUserInteractionEnabled = false
+            }
+        }
+    }
+}
 
+extension ForgetPasswordViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.animateDoneButton()
+        textField.resignFirstResponder()
+        return true
+    }
 }
